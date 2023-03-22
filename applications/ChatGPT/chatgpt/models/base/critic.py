f1a9cd38bca7f8e0aaaa8fd36d5ed5ff308c7c61
visla-maxiangxiang <114382211+visla-maxiangxiang@users.noreply.github.com>
2023-03-22 12:01:28 +0800

    Update critic.py
    指出ColossalAI与trl实现的critical的差异。

--- applications/ChatGPT/chatgpt/models/base/critic.py
+++ applications/ChatGPT/chatgpt/models/base/critic.py
@@ -41,11 +41,13 @@ class Critic(LoRAModule):
         last_hidden_states = outputs['last_hidden_state']
 
         values = self.value_head(last_hidden_states).squeeze(-1)
+        # 如果此时返回 values[-num_actions:],那么模型就能预测response中每个token的价值了，也就是Q((response+prompt)[0:i])。
+        #在trl版采用MDP进行强化学习的RLHF中就是这样实现的。
 
         if action_mask is not None and self.use_action_mask:
             num_actions = action_mask.size(1)
             prompt_mask = attention_mask[:, :-num_actions]
-            values = values[:, :-num_actions]
+            values = values[:, :-num_actions] 
             #利用prompt_mask，只计算与prompt相关的部分，也就是value只与prompt有关，与response无关。
             value = masked_mean(values, prompt_mask, dim=1)
             #输出的value是一个形状为[B]的张量，表示prompt对应状态的价值即V(prompt)。在Actor-Critical系算法里为critical(state)=critical(prompt)
