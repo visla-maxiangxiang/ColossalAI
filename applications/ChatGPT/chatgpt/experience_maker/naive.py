568da275b0fe479f1679c88089b60603b9957133
visla-maxiangxiang <114382211+visla-maxiangxiang@users.noreply.github.com>
2023-03-23 11:13:32 +0800

    Update naive.py
    对经验生成器代码进行注释

--- applications/ChatGPT/chatgpt/experience_maker/naive.py
+++ applications/ChatGPT/chatgpt/experience_maker/naive.py
@@ -7,6 +7,10 @@ from .base import Experience, ExperienceMaker
 class NaiveExperienceMaker(ExperienceMaker):
     """
     Naive experience maker.
+    利用旧的策略网络也就是initial_model生成数据。这些数据会被保存起来，用于训练新策略actor。
+    生成的经验数据会被replay_buffer进行缓存，并在强化学习过程中定期更新，保障强化学习用的数据的完全随机性。
+    因为一直使用同一批经验数据会引入偏差。PPO算法要求强化训练的数据必须是来自旧策略完全随机采样的，（蒙塔卡罗法的要求）。
+    
     """
 
     @torch.no_grad()
