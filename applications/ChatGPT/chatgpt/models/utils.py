286471d0c1b89a604b8a43b53be8b29e15c3b081
visla-maxiangxiang <114382211+visla-maxiangxiang@users.noreply.github.com>
2023-03-23 11:05:29 +0800

    Update utils.py
    对kl散度的近似算法进行注释

--- applications/ChatGPT/chatgpt/models/utils.py
+++ applications/ChatGPT/chatgpt/models/utils.py
@@ -12,7 +12,23 @@ def compute_approx_kl(log_probs: torch.Tensor,
     """
     Compute the approximate KL divergence between two distributions.
     Schulman blog: http://joschu.net/blog/kl-approx.html
-
+    我来做一个简单的解释：
+    KL(p,q) = \sum_i{p_i*log(p_i/q_i)} = EXPa~p【-log(q_i/p_i)】
+    我们发现KL有很多项构成，每一项有正有负，但总体上是正的。
+    虽然上面是一个期望表达式，理论上可以利用蒙塔卡罗法进行估算，但是如果我们采样出的样本导致q_i>p_i时，估算出的KL竟然是负的。
+    显然-log(q_i/p_i)这个表达式的方差很大，需要采样出大量的样本才能利用蒙塔卡罗法进行估算。
+    但是怎么衡量每一项对于整体的贡献呢? KL(p,q) = sum_i {distance(p_i,q_i)} 保障distance(p_i,q_i)都是正的。
+    可以这样改写:
+    KL(p,q) = \sum_i{p_i*-log(q_i/p_i)} = \sum_i{p_i*[-log(q_i/p_i) + (q_i/p_i -1)]}
+    因为\sum_i{p_i*[q_i/p_i - 1]} = sum_i{q_i - p_i} = sum_i{q_i} - sum_i{p_i} = 1-1 = 0
+    令：ratio_i=q_i/p_i ; log_ratio_i = log(q_i/p_i) 
+    则：KL(p,q) = \sum_i{p_i*[(ratio_i - 1) - log_ratio_i]} = EXPa~p【ratio_i - 1 - log_ratio_i】。
+    我们观测期望中的表达式: ratio_i - 1 - log_ratio_i，我们发现该表达式的值恒为正，并且当 q_i = p_i时值为0。
+    并且ratio_i-1-log_ratio_i的方差比 -log_ratio_i小的多，更加适合利用蒙塔卡罗法进行采样估计。
+    于是我们利用策略p只进行一次采样，得到了一个动作a_i，带入后算法后得到了 KL ~= ratio_i -1 - log_ratio_i = ratio_i.exp() - 1 - log_ratio_i
+    当然只进行一次采样所估算出的kl距离还是存在很大误差的，但至少保障了估算出的KL距离为正了，并且当p、q策略收敛且大致相等时，利用p采样出的动作非常集中kl->0，则估算的KL会非常精确。
+    因此上面的算法只能用在策略p大致收敛的场景中，
+    下面的代码就是利用上面所描述的算法进行kl估算的。
     Args:
         log_probs: Log probabilities of the new distribution.
         log_probs_base: Log probabilities of the base distribution.
@@ -35,7 +51,10 @@ def compute_reward(r: Union[torch.Tensor, float],
                    action_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
     if kl_coef <= 0.0:
         return r
+    #这里计算的并不是kl，甚至也不是kl的近似，只是kl在token真实取值上的分量。
+    #直观上的意义是kl新旧策略在sequence项概率取值距离。表征了新旧策略差异的大小。
     kl = compute_approx_kl(log_probs, log_probs_base, action_mask=action_mask)
+    #返回的奖励=RM(prompt,response) - 新旧策略的差异大小*正因子
     reward = r - kl_coef * kl
     return reward
 
@@ -45,7 +64,7 @@ def log_probs_from_logits(logits: torch.Tensor, labels: torch.Tensor) -> torch.T
     log_probs_labels = log_probs.gather(dim=-1, index=labels.unsqueeze(-1))
     return log_probs_labels.squeeze(-1)
 
-
+#有些token是无效的，计算平均时忽略掉这些token。
 def masked_mean(tensor: torch.Tensor, mask: torch.Tensor, dim: int = 1) -> torch.Tensor:
     tensor = tensor * mask
     tensor = tensor.sum(dim=dim)
